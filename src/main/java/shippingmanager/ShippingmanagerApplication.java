dd85a5845bf239a56314290c5dcd2b294087e578
Rafal Szczotka <rafalszczotka@wp.pl>
2020-11-25 16:55:19 +0000

    first commit

---
+++ src/main/java/shippingmanager/ShippingmanagerApplication.java
@@ -0,0 +1,13 @@
+package shippingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shippingmanager/ShippingmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package shippingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
