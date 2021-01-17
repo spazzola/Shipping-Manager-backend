2de5f540ca62445b4bda01657ee8a759f99397ea
Rafal Szczotka <rafalszczotka@wp.pl>
2021-01-17 13:48:01 +0100

    Fixed bug with wrong mapping OrderDrivers

--- src/main/java/shippingmanager/utility/orderdriver/OrderDriverMapper.java
+++ src/main/java/shippingmanager/utility/orderdriver/OrderDriverMapper.java
@@ -1,18 +1,33 @@
 package shippingmanager.utility.orderdriver;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import shippingmanager.utility.driver.DriverDto;
+import shippingmanager.utility.phonenumber.PhoneNumberDto;
+import shippingmanager.utility.phonenumber.PhoneNumberMapper;
+import shippingmanager.utility.plate.Plate;
+import shippingmanager.utility.plate.PlateDto;
+import shippingmanager.utility.plate.PlateMapper;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@AllArgsConstructor
 @Component
 public class OrderDriverMapper {
 
+    private PlateMapper plateMapper;
+    private PhoneNumberMapper phoneNumberMapper;
+
     public OrderDriverDto toDto(OrderDriver orderDriver) {
+        List<PlateDto> platesDto = plateMapper.toDto(orderDriver.getDriver().getPlates());
+        List<PhoneNumberDto> phoneNumbersDto = phoneNumberMapper.toDto(orderDriver.getDriver().getPhoneNumbers());
+
         DriverDto driverDto = DriverDto.builder()
                 .name(orderDriver.getDriver().getName())
                 .surname(orderDriver.getDriver().getSurname())
+                .plates(platesDto)
+                .phoneNumbers(phoneNumbersDto)
                 .build();
 
         return OrderDriverDto.builder()
