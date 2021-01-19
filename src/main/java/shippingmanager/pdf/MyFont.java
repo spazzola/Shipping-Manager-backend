11bd32fcde9eac693d69f8c1c2f872a251eaa4b4
Rafal Szczotka <rafalszczotka@wp.pl>
2021-01-19 22:36:57 +0100

    Added MyFont

--- src/main/java/shippingmanager/pdf/MyFont.java
+++ src/main/java/shippingmanager/pdf/MyFont.java
@@ -1,5 +1,9 @@
 package shippingmanager.pdf;
 
+import com.itextpdf.io.font.FontProgram;
+import com.itextpdf.io.font.FontProgramFactory;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
@@ -14,6 +18,8 @@ import static com.itextpdf.text.FontFactory.registerDirectory;
 @NoArgsConstructor
 public class MyFont {
 
+    public static final String REGULAR = "src/main/resources/fonts/montserat-regular.ttf";
+    public static final String BOLD = "src/main/resources/fonts/montserat-bold.ttf";
 
     public static Font getBoldFont(int size) throws IOException, DocumentException {
         BaseFont bf = BaseFont.createFont("fonts/montserat-bold.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
@@ -25,6 +31,16 @@ public class MyFont {
         BaseFont bf = BaseFont.createFont("fonts/montserat-regular.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
 
         return new Font(bf, 8);
+    }
+
+    public static PdfFont getRegularFont() throws IOException {
+        FontProgramFactory.createFont(REGULAR);
+        return PdfFontFactory.createFont(REGULAR, "Identity-H",true);
+    }
+
+    public static PdfFont getBolderFont() throws IOException {
+        FontProgramFactory.createFont(BOLD);
+        return PdfFontFactory.createFont(BOLD, "Identity-H",true);
     }
 
 }
