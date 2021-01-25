ac0cbb48275d870e262897f749f7194489fb7eb2
Rafal Szczotka <rafalszczotka@wp.pl>
2021-01-25 18:38:28 +0100

    Added method to extract taxes with particular value

--- src/main/java/shippingmanager/utility/product/ProductService.java
+++ src/main/java/shippingmanager/utility/product/ProductService.java
@@ -1,6 +1,7 @@
 package shippingmanager.utility.product;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -8,7 +9,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductService {
 
-    public List<Product> calculateValues(List<Product> products) {
+    public List<Product> getProductWithTax23(List<Product> products) {
+        List<Product> resultList = new ArrayList<>();
+        for (Product product : products) {
+            if (product.getTax().equals(BigDecimal.valueOf(0.23))) {
+                resultList.add(product);
+            }
+
+        }
+        return resultList;
+    }
+
+    public List<Product> getProductWithTax08(List<Product> products) {
+        List<Product> resultList = new ArrayList<>();
+        for (Product product : products) {
+            if (product.getTax().equals(BigDecimal.valueOf(0.08))) {
+                resultList.add(product);
+            }
+
+        }
+        return resultList;
+    }
+
+    public List<Product> getProductWithTax05(List<Product> products) {
+        List<Product> resultList = new ArrayList<>();
+        for (Product product : products) {
+            if (product.getTax().equals(BigDecimal.valueOf(0.05))) {
+                resultList.add(product);
+            }
+
+        }
+        return resultList;
+    }
+
+    public void calculateValues(List<Product> products) {
         for (Product product : products) {
             BigDecimal valueWithoutTax = product.getPriceWithoutTax().multiply(product.getQuantity());
             product.setValueWithoutTax(valueWithoutTax);
@@ -16,7 +50,6 @@ public class ProductService {
             product.setValueWithTax(valueWithTax);
             product.setTaxValue(valueWithTax.subtract(valueWithoutTax));
         }
-        return products;
     }
 
     private BigDecimal calculateValueWithTax(BigDecimal valueWithoutTax, BigDecimal tax) {
