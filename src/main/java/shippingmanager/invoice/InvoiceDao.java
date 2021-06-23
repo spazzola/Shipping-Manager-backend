c66c54933de19c4d83cccc49949b0bd1b7bef2ee
Rafal Szczotka <rafalszczotka@wp.pl>
2021-06-23 13:53:55 +0200

    Added to InvoiceDao finding Invoice by month and year

--- src/main/java/shippingmanager/invoice/InvoiceDao.java
+++ src/main/java/shippingmanager/invoice/InvoiceDao.java
@@ -1,9 +1,20 @@
 package shippingmanager.invoice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Optional;
 
 @Repository
 public interface InvoiceDao extends JpaRepository<Invoice, Long> {
+
+    @Query(value = "SELECT * FROM invoices i " +
+            "WHERE MONTH(i.issued_Date) = ?1 AND YEAR(i.issued_Date) = ?2 " +
+            "ORDER BY invoice_id DESC LIMIT 1",
+            nativeQuery = true)
+    Optional<Invoice> findByMonthAndYear(int month, int year);
 
 }
