79ac7df524356d39dae320e21a424a1695b99623
Rafal Szczotka <rafalszczotka@wp.pl>
2021-06-23 13:43:57 +0200

    Added to OrderDao finding Order by month and year

--- src/main/java/shippingmanager/order/OrderDao.java
+++ src/main/java/shippingmanager/order/OrderDao.java
@@ -1,6 +1,7 @@
 package shippingmanager.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +11,11 @@ public interface OrderDao extends JpaRepository<Order, Long> {
 
     Order findByOrderNumber(String orderNumber);
     Optional<Order> findTopByOrderByIdDesc();
+
+    @Query(value = "SELECT * FROM orders o " +
+            "WHERE MONTH(o.created_Date) = ?1 AND YEAR(o.created_Date) = ?2 " +
+            "ORDER BY order_id DESC LIMIT 1",
+            nativeQuery = true)
+    Optional<Order> findByMonthAndYear(int month, int year);
 
 }
