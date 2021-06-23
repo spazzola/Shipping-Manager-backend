a624b0e09734d1033bc6566a156ab6db2af1f945
Rafal Szczotka <rafalszczotka@wp.pl>
2021-06-23 13:42:24 +0200

    Refactor MyFont: changed font's path to relative

--- src/main/java/shippingmanager/pdf/MyFont.java
+++ src/main/java/shippingmanager/pdf/MyFont.java
@@ -3,40 +3,26 @@ package shippingmanager.pdf;
 import com.itextpdf.io.font.FontProgramFactory;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
-import javafx.application.Application;
 import lombok.NoArgsConstructor;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
 
 @NoArgsConstructor
 public class MyFont {
 
-    //private static final String REGULAR = "src/main/resources/fonts/montserat-regular.ttf";
-    //private static final String BOLD = "src/main/resources/fonts/montserat-bold.ttf";
+    private static final String REGULAR = "src/main/resources/fonts/montserat-regular.ttf";
+    private static final String BOLD = "src/main/resources/fonts/montserat-bold.ttf";
+
 
     public static PdfFont getRegularFont() throws IOException {
-        String path = loadRegularFontPath();
-        FontProgramFactory.createFont(path);
-        return PdfFontFactory.createFont(path, "Identity-H",true);
+        FontProgramFactory.createFont(REGULAR);
+        return PdfFontFactory.createFont(REGULAR, "Identity-H",true);
     }
 
     public static PdfFont getBoldFont() throws IOException {
-        String path = loadBoldFontPath();
-        FontProgramFactory.createFont(path);
-        return PdfFontFactory.createFont(path, "Identity-H",true);
-    }
-
-    private static String loadRegularFontPath() {
-        return MyFont.class.getClassLoader().getResource("fonts/montserat-regular.ttf").getPath();
-    }
-
-    private static String loadBoldFontPath() {
-        return MyFont.class.getClassLoader().getResource("fonts/montserat-bold.ttf").getPath();
+        FontProgramFactory.createFont(BOLD);
+        return PdfFontFactory.createFont(BOLD, "Identity-H",true);
     }
 
 }
