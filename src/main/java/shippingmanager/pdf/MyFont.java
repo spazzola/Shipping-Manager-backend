7d99c59f2785eb1b9a949ab69090cac11635c374
Rafal Szczotka <rafalszczotka@wp.pl>
2021-06-24 11:35:19 +0200

    Changed path to font to relative

--- src/main/java/shippingmanager/pdf/MyFont.java
+++ src/main/java/shippingmanager/pdf/MyFont.java
@@ -11,18 +11,28 @@ import java.io.IOException;
 @NoArgsConstructor
 public class MyFont {
 
-    private static final String REGULAR = "src/main/resources/fonts/montserat-regular.ttf";
-    private static final String BOLD = "src/main/resources/fonts/montserat-bold.ttf";
+    //private static final String REGULAR = "src/main/resources/fonts/montserat-regular.ttf";
+    //private static final String BOLD = "src/main/resources/fonts/montserat-bold.ttf";
 
 
     public static PdfFont getRegularFont() throws IOException {
-        FontProgramFactory.createFont(REGULAR);
-        return PdfFontFactory.createFont(REGULAR, "Identity-H",true);
+        String path = loadRegularFontPath();
+        FontProgramFactory.createFont(path);
+
+        return PdfFontFactory.createFont(path, "Identity-H",true);
     }
 
     public static PdfFont getBoldFont() throws IOException {
-        FontProgramFactory.createFont(BOLD);
-        return PdfFontFactory.createFont(BOLD, "Identity-H",true);
+        String path = loadBoldFontPath();
+        FontProgramFactory.createFont(path);
+        return PdfFontFactory.createFont(path, "Identity-H",true);
     }
 
+    private static String loadRegularFontPath() {
+        return MyFont.class.getClassLoader().getResource("fonts/montserat-regular.ttf").getPath();
+    }
+
+    private static String loadBoldFontPath() {
+        return MyFont.class.getClassLoader().getResource("fonts/montserat-bold.ttf").getPath();
+    }
 }
